fe347e00657535de67cd7d6caa58da1774990911
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-08-23 01:33:38 +0530

    chore: Update AppBarMain component to use new background color

--- weatherapp/src/components/AppBarMain.jsx
+++ weatherapp/src/components/AppBarMain.jsx
@@ -4,11 +4,11 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const AppBarMain = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: "#1E1E1E" }}>
+    <AppBar position="static" sx={{ backgroundColor: "#202124" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         {/* Logo */}
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My News App
+          Serendib News
         </Typography>
 
         {/* Search Bar */}
