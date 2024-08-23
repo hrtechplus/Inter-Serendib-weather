f1f60b23c0db49e45064bc8912239be42f3afff3
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-08-23 12:50:43 +0530

    chore: Update AppBarMain component to use SearchBar for search functionality

--- weatherapp/src/components/AppBarMain.jsx
+++ weatherapp/src/components/AppBarMain.jsx
@@ -2,23 +2,36 @@ import React from "react";
 import { AppBar, Toolbar } from "@mui/material";
 import SearchBar from "./SearchBar";
 import TextLine from "./TextLine";
-
+import RefreshSharpIcon from "@mui/icons-material/RefreshSharp";
 const AppBarMain = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: "#202124", boxShadow: 0 }}>
+    <AppBar
+      position="static"
+      sx={{
+        display: "flex",
+        backgroundColor: "#202124",
+        alignItems: "space-around",
+        boxShadow: 0,
+        lineHeight: 1,
+        paddingY: 2,
+      }}
+    >
       <Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
         {/* Logo --1 */}
-        <TextLine
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1 }}
-          text="Serendib News"
-        />
+        <TextLine variant="h6" text="Serendib" />
 
         {/* Search Bar --1 */}
         <SearchBar />
-        {/* Search Bar --1 */}
-        <SearchBar />
+
+        <RefreshSharpIcon
+          sx={{
+            color: "#BDC1C6",
+            border: 1,
+            padding: 1,
+            marginX: 1,
+            borderRadius: 2,
+          }}
+        />
       </Toolbar>
     </AppBar>
   );
--- weatherapp/src/components/SearchBar.jsx
+++ weatherapp/src/components/SearchBar.jsx
@@ -1,15 +1,18 @@
 import { Box, InputBase } from "@mui/material";
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
+
 export default function SearchBar() {
   return (
     <Box
       sx={{
         display: "flex",
         alignItems: "center",
+        marginX: 2,
         backgroundColor: "#444",
         borderRadius: 2,
         padding: 1,
+        width: { xs: "100%", sm: "50%" },
       }}
     >
       <SearchIcon sx={{ color: "#BDC1C6 " }} />
